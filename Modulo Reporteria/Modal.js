1e32992f7e3add70f34eac929ab06a275edd0411
martinpez <perezmercadomartin@gmail.com>
2026-08-27 17:32:08 -0500

    FEAT: Modulo de reportaje de bugs

---
+++ Modulo Reporteria/Modal.js
@@ -0,0 +1,62 @@
+function load_modalBugs() {
+  var modal = document.getElementById('modalBugsTG');
+  if (!modal) return;
+
+  var usuario = modal.querySelector('#_bugsTG-usuario');
+  var fechaInput = modal.querySelector('#_bugsTG-fecha');
+  var closeBtn = modal.querySelector('#_bugsTG-close-btn');
+  var cancelBtn = modal.querySelector('#_bugsTG-btn-cancel');
+  var submitBtn = modal.querySelector('#_bugsTG-btn-submit');
+  var uploadZone = modal.querySelector('#_bugsTG-upload-zone');
+  var fileInput = modal.querySelector('#_bugsTG-file');
+  var date = sessionStorage.date || '';
+  var userName = localStorage.getItem('userName') || '';
+  if (usuario) usuario.value = userName;
+  if (date) {
+    if (fechaInput) {
+      fechaInput.value = date;
+    }
+  }
+
+  modal.showModal();
+
+  modal.addEventListener('click', function (event) {
+    if (event.target === modal) modal.close();
+  });
+
+  if (closeBtn) {
+    closeBtn.addEventListener('click', function () { modal.close(); });
+  }
+
+  if (cancelBtn) {
+    cancelBtn.addEventListener('click', function () { modal.close(); });
+  }
+
+  if (submitBtn) {
+    submitBtn.addEventListener('click', function () {
+      var modulo = modal.querySelector('#_bugsTG-modulo').value;
+      var descripcion = modal.querySelector('#_bugsTG-descripcion').value;
+      var file = fileInput && fileInput.files.length > 0 ? fileInput.files[0] : null;
+
+      console.log('Reporte de bug:', {
+        usuario: usuario ? usuario.value : '',
+        fecha: fechaInput ? fechaInput.value : '',
+        modulo: modulo,
+        descripcion: descripcion,
+        evidencia: file ? file.name : null
+      });
+
+      modal.close();
+    });
+  }
+
+  if (uploadZone && fileInput) {
+    uploadZone.addEventListener('click', function () { fileInput.click(); });
+    fileInput.addEventListener('change', function () {
+      if (fileInput.files.length > 0) {
+        uploadZone.querySelector('p').innerHTML =
+          'Archivo: <strong>' + fileInput.files[0].name + '</strong>';
+      }
+    });
+  }
+}
---
+++ Modulo Reporteria/agregar-icono-bug.js
@@ -0,0 +1,50 @@
+(function () {
+  'use strict';
+
+  function insertBugIcon() {
+    var menu = document.getElementById('sidebar-menu');
+    if (!menu || menu.querySelector('.fa-bug')) return;
+
+    var separatorHTML = '<li class="nav-item nav-link-ref ng-star-inserted" style="border-left: 2px solid rgba(0, 44, 118, 0.35); align-self: center; height: 40px; padding: 0; margin: 0px; margin-bottom: 0px; border-bottom-style: solid; border-bottom-width: 0px; bottom: 4px;"></li>';
+    var bugIconHTML = '<li class="nav-item nav-link-ref ng-star-inserted"><a class="nav-link" style="color: rgb(0, 44, 118); padding-left: 5px;"><i class="fa fa-bug link-icon" aria-hidden="true" style="font-size: 20px; margin-bottom: 20px; left: 6px; right: 0px;"></i></a><ul class="nav-second-level sidebar-nav sub-menu" hidden=""></ul></li>';
+
+    menu.insertAdjacentHTML('beforeend', separatorHTML + bugIconHTML);
+
+    var bugIconEl = menu.querySelector('.fa-bug');
+
+    var bugLink = bugIconEl ? bugIconEl.closest('.nav-link') : null;
+    if (bugLink) {
+      bugLink.style.cursor = 'pointer';
+      bugLink.addEventListener('click', function () {
+          load_modalBugs();
+      });
+    }
+  }
+
+  function init() {
+    var menu = document.getElementById('sidebar-menu');
+    if (menu) {
+      insertBugIcon();
+      return;
+    }
+    var observer = new MutationObserver(function (mutations) {
+      for (var i = 0; i < mutations.length; i++) {
+        var addedNodes = mutations[i].addedNodes;
+        for (var j = 0; j < addedNodes.length; j++) {
+          if (addedNodes[j].id === 'sidebar-menu' || (addedNodes[j].querySelector && addedNodes[j].querySelector('#sidebar-menu'))) {
+            insertBugIcon();
+            observer.disconnect();
+            return;
+          }
+        }
+      }
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
+})();
